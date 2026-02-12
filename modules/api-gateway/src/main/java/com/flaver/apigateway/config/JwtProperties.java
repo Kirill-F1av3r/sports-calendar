a96bfc7fa03bbb845e4a9440693f182a09e8e931
Kirill-F1av3r <f1av3r@yandex.ru>
2026-02-13 00:31:03 +0300

    A minimal version of the api-gateway has been created

---
+++ modules/api-gateway/src/main/java/com/flaver/apigateway/config/JwtProperties.java
@@ -0,0 +1,23 @@
+package com.flaver.apigateway.config;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@Validated
+@ConfigurationProperties(prefix = "security.jwt")
+public class JwtProperties {
+
+    @NotBlank
+    private String secret;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
---
+++ modules/api-gateway/src/main/java/com/flaver/apigateway/service/ProxyService.java
@@ -0,0 +1,36 @@
+package com.flaver.apigateway.service;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class ProxyService {
+
+    private final RestClient restClient;
+
+    public ProxyService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public ResponseEntity<byte[]> forward(String targetUrl,
+                                          HttpMethod method,
+                                          HttpHeaders headers,
+                                          byte[] body) {
+        HttpHeaders headersToForward = new HttpHeaders();
+        headersToForward.addAll(headers);
+        headersToForward.remove(HttpHeaders.HOST);
+        headersToForward.remove(HttpHeaders.CONTENT_LENGTH);
+
+        RestClient.RequestBodySpec requestSpec = restClient
+                .method(method)
+                .uri(targetUrl)
+                .headers(httpHeaders -> httpHeaders.addAll(headersToForward));
+
+        RestClient.ResponseSpec responseSpec = body != null
+                ? requestSpec.body(body).retrieve()
+                : requestSpec.retrieve();
+
+        return responseSpec.toEntity(byte[].class);
+    }
+}
