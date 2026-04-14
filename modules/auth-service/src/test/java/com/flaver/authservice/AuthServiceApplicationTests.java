4dff41fbfdcd2ae99263674bf21c8866fb7848cd
Kirill-F1av3r <f1av3r@yandex.ru>
2026-04-15 00:20:17 +0300

    postgres has been replaced with h2 for testing

--- modules/auth-service/src/test/java/com/flaver/authservice/AuthServiceApplicationTests.java
+++ modules/auth-service/src/test/java/com/flaver/authservice/AuthServiceApplicationTests.java
@@ -3,7 +3,11 @@ package com.flaver.authservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:auth_test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class AuthServiceApplicationTests {
 
     @Test
